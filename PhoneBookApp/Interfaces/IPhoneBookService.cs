21f0a9d0de54c36febbe690358b0410ceae16ff8
Ксения Миронова <ksyusha1908@gmail.com>
2021-11-21 08:59:56 +0700

    Удалена ненужная информация из файлов Index.razor, NavMenu.razor и Startup.cs. Добавлены файлы AppContext.cs, DapperService.cs, PhoneBookService.cs, PhoneBook.cs, IDapperService.sc, IPhoneBookService.cs.

---
+++ PhoneBookApp/Interfaces/IPhoneBookService.cs
@@ -0,0 +1,18 @@
+﻿using PhoneBookApp.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+
+namespace PhoneBookApp.Interfaces
+{
+    public interface IPhoneBookService
+    {
+        Task<int> Create(PhoneBook phoneBook);
+        Task<int> Delete(int Id);
+        Task<int> Count(string search);
+        Task<int> Update(PhoneBook phoneBook);
+        Task<PhoneBook> GetById(int Id);
+        Task<List<PhoneBook>> ListAll(int skip, int take,
+            string orderBy, string direction, string search);
+    }
+}
